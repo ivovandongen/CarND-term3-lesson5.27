ab6bfd2b748178f47e2fd4a7d273bea87da5df56
Ivo van Dongen <info@ivovandongen.nl>
2018-09-28 13:25:26 +0300

    split off core library from main executable

---
+++ src/core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ivo van Dongen on 28/09/2018.
+//
+
+#include "core.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <core.hpp>
+
 #include <iostream>
 
 int main() {
+    Core core;
     std::cout << "Template" << std::endl;
 }
 
