94ae10300e39cfe5aad02a05fbd081d034ff0a01
Ivo van Dongen <info@ivovandongen.nl>
2018-09-28 13:24:54 +0300

    moved Eigen specifics into cmake file

--- deps/eigen.cmake
+++ deps/eigen.cmake
@@ -1,3 +1,8 @@
 add_library(Eigen INTERFACE)
 
 target_include_directories(Eigen INTERFACE ${CMAKE_SOURCE_DIR}/deps/Eigen/Eigen)
+
+if (UNIX)
+    # Silence warnings in Eigen
+    add_compile_options(-Wno-deprecated-register)
+endif()
