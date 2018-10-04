45514245da1d392a7d68b9801e69a61c3e1c5f2f
Ivo van Dongen <info@ivovandongen.nl>
2018-10-04 15:24:22 +0300

    Eigen - proper namespace for headers

--- deps/Eigen.cmake
+++ deps/Eigen.cmake
@@ -2,7 +2,7 @@ include_guard_x(__EIGEN__)
 
 add_library(Eigen INTERFACE)
 
-target_include_directories(Eigen INTERFACE ${CMAKE_SOURCE_DIR}/deps/Eigen/Eigen)
+target_include_directories(Eigen INTERFACE ${CMAKE_SOURCE_DIR}/deps/Eigen)
 
 if (UNIX)
     # Silence warnings in Eigen
