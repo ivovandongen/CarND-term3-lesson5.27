d07f179f9072a734599ef41f558182808ae3e3e9
Ivo van Dongen <info@ivovandongen.nl>
2018-10-07 13:15:10 +0300

    cmake fix include_guard properly

--- cmake/include_guard.cmake
+++ cmake/include_guard.cmake
@@ -1,10 +1,12 @@
 # Include guard (include_guard is not included in older cmake version)
 macro(INCLUDE_GUARD_X GUARD_NAME)
 
-    if(GUARD_NAME)
+    GET_PROPERTY(GUARD_VALUE GLOBAL PROPERTY ${GUARD_NAME})
+
+    if(GUARD_VALUE)
         return()
     endif()
 
-    set(${GUARD_NAME} TRUE)
+    SET_PROPERTY(GLOBAL PROPERTY ${GUARD_NAME} TRUE)
 
 endmacro(INCLUDE_GUARD_X)
